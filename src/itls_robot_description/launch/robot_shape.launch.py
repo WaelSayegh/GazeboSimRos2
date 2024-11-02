88ab5bfa26b89f6179f1d1be40a9d9142a8ac1c5
Wael Alsayegh <wael.alsayegh@inmind.ai>
2024-11-02 21:47:33 +0000

    ignition edits and gazebo plugin calibrations

--- src/itls_robot_description/launch/robot_shape.launch.py
+++ src/itls_robot_description/launch/robot_shape.launch.py
@@ -17,16 +17,16 @@ def generate_launch_description():
     ros_gz_sim_package = get_package_share_directory('ros_gz_sim')
 
     # Set the path to the URDF file
-    urdf_model = os.path.join(desc_package, "urdf/parts/itls_main.urdf.xacro")
+    urdf_model = os.path.join(desc_package, "urdf/itls_main.urdf.xacro")
     robot_desc_config = Command(['xacro ', urdf_model, ' sim_mode:=true'])
-    params = {'robot_description': robot_desc_config, 'use_sim_time': True}
+    robot_desc_param = {'robot_description': robot_desc_config, 'use_sim_time': True}
 
     # Publishes the robot description, excluding joints that can be calibrated at runtime
     robot_state_publisher_node = Node(
         name="robot_state_publisher",
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[params],
+        parameters=[robot_desc_param],
     )
 
     # Setup to launch the simulator and Gazebo world
@@ -42,29 +42,24 @@ def generate_launch_description():
         arguments=['-topic', 'robot_description', '-name', 'itls_robot_gz', '-z', '0.14'],
         output='screen')
 
-    # # Initialize Gazebo Bridge
-    # gz_bridge = Node(
-    #     package = "ros_ign_bridge",
-    #     executable = "parameter_bridge",
-    #     name = "gazebo_bridge",
-    #     arguements
-    # )
-
-    # # Bridge ROS topics and Gazebo messages for establishing communication
-    # bridge = Node(
-    #     package='ros_gz_bridge',
-    #     executable='parameter_bridge',
-    #     parameters=[{
-    #         'config_file': os.path.join(pkg_project_bringup, 'config', 'ros_gz_example_bridge.yaml'),
-    #         'qos_overrides./tf_static.publisher.durability': 'transient_local',
-    #     }],
-    #     output='screen'
-    # )
+    # Initialize Gazebo Bridge
+    gz_bridge_config = os.path.join(desc_package, 'config', 'gazebo_bridge_config.yaml')
+    gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        name="gazebo_bridge",
+        parameters=[{
+            'config_file': gz_bridge_config,
+            'qos_overrides./tf_static.publisher.durability': 'transient_local',
+        }],
+        output='screen'
+    )
 
     return LaunchDescription([
         robot_state_publisher_node,
         gz_sim,
-        gz_spawner
+        gz_spawner,
+        gz_bridge
         # bridge,
         # robot_state_publisher,
         # rviz
